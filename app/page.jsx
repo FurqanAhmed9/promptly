0955c773762a4fdcbc65fa2686f3cfc4408cb4e3
Furqan Ahmed <furqanahmedshaikh5@gmail.com>
2023-08-13 18:31:02 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center block">
-        Explore And Exchange
+        Discover And Share
         <br className="max-sm:hidden" />
         <span className="blue_gradient text-center">AI Powered Prompts</span>
       </h1>
